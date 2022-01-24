99129a7920f6ade20f52320a1c5f5b29238b1e90
jennyli12 <35435647+jennyli12@users.noreply.github.com>
2022-01-24 13:45:35 -0500

    File processing

--- process.py
+++ process.py
@@ -4,13 +4,25 @@ new = {}
 for bucket in range(-128, 170):
     for rd in ['P', 'F', 'T', 'R']:
         new[rd + str(bucket)] = {}
-with open('avgstrategy.pickle', 'rb') as f:
-    strategy_map = pickle.load(f)
-    for s in strategy_map:
+
+# with open('avgstrategy.pickle', 'rb') as f:
+#     strategy_map = pickle.load(f)
+#     for s in strategy_map:
+#         idx = -1
+#         while s[idx] not in {'P', 'F', 'T', 'R'}:
+#             idx -= 1
+#         new[s[idx:]][s] = strategy_map[s]
+
+with open('nodeMap.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        text = line.split(":")
+        infoset_str = text[0]
+        avg_strategy = eval(text[1].strip())
         idx = -1
-        while s[idx] not in {'P', 'F', 'T', 'R'}:
+        while infoset_str[idx] not in {'P', 'F', 'T', 'R'}:
             idx -= 1
-        new[s[idx:]][s] = strategy_map[s]
+        new[infoset_str[idx:]][infoset_str] = avg_strategy
 
 for entry in new:
     if len(new[entry]) > 0:
